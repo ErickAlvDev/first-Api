f77ebd23c34ae8c0fcb2a6a6dee5f95a54bd1375
ErickAlvDev <alvaradoerick26@gmail.com>
2024-10-28 07:42:00 -0300

    Initial commit

---
+++ src/test/java/me/ErickAlvDev/first_Api/FirstApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ErickAlvDev.first_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
